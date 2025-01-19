f5ddc61a3cfef27de39d65a4ed08fcecda6693e2
Brian Byun <brianubyun@gmail.com>
2025-01-18 22:21:17 -0800

    Ran npm run format

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,15 +1,12 @@
 const Navigation = () => {
   return (
-    <nav className="bg-hsa-pink-200 font-serif">  
-      <div className="flex text-white py-8 mx-auto"> 
+    <nav className="bg-hsa-pink-200 font-serif">
+      <div className="mx-auto flex py-8 text-white">
         <div className="flex items-center">
-          <img 
-            src="/hsaImage.svg"
-            className="ml-8 w-20 h-20 mr-8"
-          /> 
+          <img src="/hsaImage.svg" className="ml-8 mr-8 h-20 w-20" />
           <div className="text-5xl"> HSA</div>
-        </div>  
-        <div className="flex items-center ml-20 text-3xl space-x-14">
+        </div>
+        <div className="ml-20 flex items-center space-x-14 text-3xl">
           <div className="">Home</div>
           <div className="">Board</div>
           <div className="">Events</div>
