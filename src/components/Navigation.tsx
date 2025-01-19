a3f053c7945b901ac4ebbc51b8ad6dd174d864e6
Brian Byun <brianubyun@gmail.com>
2025-01-18 22:18:49 -0800

    Created navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,24 @@
 const Navigation = () => {
-  return <div className="bg-hsa-pink-200">Navigation</div>;
+  return (
+    <nav className="bg-hsa-pink-200 font-serif">  
+      <div className="flex text-white py-8 mx-auto"> 
+        <div className="flex items-center">
+          <img 
+            src="/hsaImage.svg"
+            className="ml-8 w-20 h-20 mr-8"
+          /> 
+          <div className="text-5xl"> HSA</div>
+        </div>  
+        <div className="flex items-center ml-20 text-3xl space-x-14">
+          <div className="">Home</div>
+          <div className="">Board</div>
+          <div className="">Events</div>
+          <div className="">Calendar</div>
+          <div className="">Gallery</div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navigation;
