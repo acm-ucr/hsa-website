0c4a298996bc664b6f6f9110c709955600587442
Brian Byun <brianubyun@gmail.com>
2025-01-21 14:25:26 -0800

    Added .webp extension and changed spacing of nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,15 +3,15 @@ const Navigation = () => {
     <nav className="bg-hsa-pink-200 font-serif">
       <div className="mx-auto flex py-8 text-white">
         <div className="flex items-center">
-          <img src="/hsaImage.svg" className="ml-8 mr-8 h-20 w-20" />
-          <div className="text-5xl"> HSA</div>
+          <img src="/hsaImage.webp" className="ml-12 mr-8 h-20 w-20" />
+          <div className="text-5xl ml-8"> HSA</div>
         </div>
-        <div className="ml-20 flex items-center space-x-14 text-3xl">
-          <div className="">Home</div>
-          <div className="">Board</div>
-          <div className="">Events</div>
-          <div className="">Calendar</div>
-          <div className="">Gallery</div>
+        <div className="ml-60 flex items-center space-x-36 text-3xl">
+          <a href="\home" className = "hover:text-hsa-pink-500"> Home </a>
+          <a href="\board" className = "hover:text-hsa-pink-500"> Board </a>
+          <a href="\events" className = "hover:text-hsa-pink-500"> Events </a>
+          <a href="\calendar" className = "hover:text-hsa-pink-500"> Calendar </a>
+          <a href="\gallery" className = "hover:text-hsa-pink-500"> Gallery </a>
         </div>
       </div>
     </nav>
