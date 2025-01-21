c2669ddf19ab3fb50a73a31d1b097e38186bc480
daniyalbhat34 <daniyalbhat34@gmail.com>
2025-01-21 14:05:10 -0800

    Used header in different files

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/Header";
 const Page = () => {
   return (
     <>
-      <Header />
+      <Header title="EVENTS" />
       <FriendshipGames />
       <Banquet />
       <HmongNewYear />
