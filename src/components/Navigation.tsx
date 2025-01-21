9cde019457dcc7b4cc3b0cd8170317d2a1f01d74
Brian Byun <brianubyun@gmail.com>
2025-01-21 14:25:50 -0800

    forgot to run npm format

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,14 +4,29 @@ const Navigation = () => {
       <div className="mx-auto flex py-8 text-white">
         <div className="flex items-center">
           <img src="/hsaImage.webp" className="ml-12 mr-8 h-20 w-20" />
-          <div className="text-5xl ml-8"> HSA</div>
+          <div className="ml-8 text-5xl"> HSA</div>
         </div>
         <div className="ml-60 flex items-center space-x-36 text-3xl">
-          <a href="\home" className = "hover:text-hsa-pink-500"> Home </a>
-          <a href="\board" className = "hover:text-hsa-pink-500"> Board </a>
-          <a href="\events" className = "hover:text-hsa-pink-500"> Events </a>
-          <a href="\calendar" className = "hover:text-hsa-pink-500"> Calendar </a>
-          <a href="\gallery" className = "hover:text-hsa-pink-500"> Gallery </a>
+          <a href="\home" className="hover:text-hsa-pink-500">
+            {" "}
+            Home{" "}
+          </a>
+          <a href="\board" className="hover:text-hsa-pink-500">
+            {" "}
+            Board{" "}
+          </a>
+          <a href="\events" className="hover:text-hsa-pink-500">
+            {" "}
+            Events{" "}
+          </a>
+          <a href="\calendar" className="hover:text-hsa-pink-500">
+            {" "}
+            Calendar{" "}
+          </a>
+          <a href="\gallery" className="hover:text-hsa-pink-500">
+            {" "}
+            Gallery{" "}
+          </a>
         </div>
       </div>
     </nav>
