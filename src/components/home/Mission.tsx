32ec3d3fe68a4b98e3f64fbe647501a05219cdc3
Ayush <68819610+akothule@users.noreply.github.com>
2025-01-23 14:08:44 -0800

    Finished Mission Text Section

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -1,5 +1,25 @@
+import hsa4 from "@/public/assets/hsa4.svg";
+
 const Mission = () => {
-  return <div>Our Mission</div>;
+  return (
+    <div className="flex items-center gap-1 p-10">
+      <div className="flex-col gap-1">
+        <div className="w-1/2 p-5 text-left font-serif text-6xl text-blue-800">
+          Our Mission
+        </div>
+        <div className="w-full p-5 text-left text-2xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </div>
+      </div>
+      {/* <div className="w-1/2 p-5 text-right text-4xl">placeholder</div> */}
+      <div className="flex w-1/2 items-center justify-center">
+        <img src={hsa4.src} alt="HSA" className="w-3/4" />
+      </div>
+    </div>
+  );
 };
 
 export default Mission;
