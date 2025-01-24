499b3eeb6195fdfa122610cee09f725b1f4da202
daniyalbhat34 <daniyalbhat34@gmail.com>
2025-01-24 00:37:01 -0800

    Edited the header a little bit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,19 +7,15 @@ type props = {
 
 const Header = ({ title }: props) => {
   return (
-    <div className="pt-y flex flex-row justify-between border-t-4 border-indigo-600 bg-hsa-gray-100">
-      <div className="flex flex-col justify-end">
-        <div className="flex flex-row justify-center">
-          <Image src={hsa5} alt="HSA5" className="w-1/2" />
-        </div>
+    <div className="flex flex-row justify-between border-t-4 border-indigo-600 bg-hsa-gray-100">
+      <div className="mt-12 flex flex-row justify-center">
+        <Image src={hsa5} alt="HSA5" className="w-[35%]" />
       </div>
-      <div className="flex flex-col self-center">
-        <div className="h-5 w-full pr-10 text-4xl">{title}</div>
+      <div className="ml-5 flex flex-col justify-center">
+        <div className="mb-5 h-5 w-full pr-10 text-4xl">{title}</div>
       </div>
-      <div className="flex flex-col justify-start">
-        <div className="flex flex-row justify-center">
-          <Image src={hsa5} alt="HSA5" className="w-1/2" />
-        </div>
+      <div className="mb-12 flex flex-row justify-center">
+        <Image src={hsa5} alt="HSA5" className="w-[35%]" />
       </div>
     </div>
   );
