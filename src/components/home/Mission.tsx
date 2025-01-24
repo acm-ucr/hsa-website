a889cedf1f0c7df9b9739ae0bbd6f42da4b8653b
Ayush <68819610+akothule@users.noreply.github.com>
2025-01-23 17:05:43 -0800

    Fixed Image Alignment to be Slightly Offscreen

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -2,20 +2,19 @@ import hsa4 from "@/public/assets/hsa4.svg";
 
 const Mission = () => {
   return (
-    <div className="flex items-center gap-1 p-10">
-      <div className="flex-col gap-1">
-        <div className="w-1/2 p-5 text-left font-serif text-6xl text-blue-800">
+    <div className="flex w-full justify-between">
+      <div className="w-3/4 flex-col p-10">
+        <div className="ml-5 p-5 text-left font-serif text-6xl text-blue-800">
           Our Mission
         </div>
-        <div className="w-full p-5 text-left text-2xl">
+        <div className="ml-5 p-5 text-left text-2xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </div>
       </div>
-      {/* <div className="w-1/2 p-5 text-right text-4xl">placeholder</div> */}
-      <div className="flex w-1/2 items-center justify-center">
+      <div className="relative left-6 flex justify-end">
         <img src={hsa4.src} alt="HSA" className="w-3/4" />
       </div>
     </div>
