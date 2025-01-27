f037288fc28cd75d83a0e9281fc4e261a1743faa
daniyalbhat34 <daniyalbhat34@gmail.com>
2025-01-26 21:50:34 -0800

    Final HEADER PUSH

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,15 @@ type props = {
 
 const Header = ({ title }: props) => {
   return (
-    <div className="flex flex-row justify-between border-t-4 border-indigo-600 bg-hsa-gray-100">
+    <div className="flex flex-row justify-between bg-hsa-gray-100">
       <div className="mt-12 flex flex-row justify-center">
-        <Image src={hsa5} alt="HSA5" className="w-[35%]" />
+        <Image src={hsa5} alt="HSA5" className="w-1/2" />
       </div>
       <div className="ml-5 flex flex-col justify-center">
         <div className="mb-5 h-5 w-full pr-10 text-4xl">{title}</div>
       </div>
       <div className="mb-12 flex flex-row justify-center">
-        <Image src={hsa5} alt="HSA5" className="w-[35%]" />
+        <Image src={hsa5} alt="HSA5" className="w-1/2" />
       </div>
     </div>
   );
