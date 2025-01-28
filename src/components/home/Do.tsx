d528f4ef69ad79b33b8a0e7e6b5b7d76403226a0
tanishqmano <tanishqmanohar@gmail.com>
2025-01-28 01:42:13 -0800

    made changed for my Do.tsx

--- src/components/home/Do.tsx
+++ src/components/home/Do.tsx
@@ -1,5 +1,34 @@
+
+import Image from "next/image";
+import border from "@/public/border.svg"
+
+
 const Do = () => {
-  return <div>What we do</div>;
+  return( 
+  <div className="w-full flex-row rounded-lg bg-hsa-tan-100">
+      <div className = "w-3/4 h-3/4 flex-row p-8 space-y-5  ">
+        <div className = "w-full text-4xl font-bold font-songMyung  text-hsa-blue-100">What We Do</div>
+        <div className="w-full h-3/4
+         text-md font-semibold text-hsa-gray-300 font-open-sans">
+        <p>Our team is made up of a group of talented individuals who are passionate about technology and innovation.</p>
+        </div>
+      </div>
+      <div className="w-full box-content-1 space-y-5">
+      <Image src={border} alt="add image description here" className = "w-full object-contain"/>
+
+      <div className="w-full h-16 bg-hsa-gray-200"></div>
+      </div>
+    
+  </div>
+    
+    
+    
+    
+    
+    
+    
+    
+  );
 };
 
 export default Do;
