8e9d8d346db39004f91688684e10a6adb3fc431b
tanishqmano <tanishqmanohar@gmail.com>
2025-01-28 15:12:31 -0800

    made more changes

--- src/components/home/Do.tsx
+++ src/components/home/Do.tsx
@@ -4,14 +4,14 @@ import hsaImage from "@/public/hsaImage.webp";
 
 const Do = () => {
   return (
-    <div className="w-full flex-row rounded-lg bg-hsatan-100">
+    <div className="bg-hsatan-100 w-full flex-row rounded-lg">
       <div className="ml-5 h-3/4 w-3/4 space-y-5 p-10">
-        <div className="font-songMyung w-full text-6xl font-Semibold text-hsa-blue-100">
+        <div className="font-Semibold w-full font-songMyung text-6xl text-hsa-blue-100">
           What We Do
         </div>
         <div className="text-2x1 font-open-sans font-semibold text-hsa-gray-300">
           <p>
-            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww           
+            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
             wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
             wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
             wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
@@ -28,12 +28,7 @@ const Do = () => {
         />
 
         <div className="w-full bg-hsa-gray-200">
-          <Image src={hsaImage} alt="HSA" className="h-16 w-16 p-1"/>
-
-
-
-
-
+          <Image src={hsaImage} alt="HSA" className="h-16 w-16 p-1" />
         </div>
       </div>
     </div>
