073e85e13cbeda822ca97af98d8d088946f8e75a
tanishqmano <tanishqmanohar@gmail.com>
2025-01-28 01:44:51 -0800

    made changes

--- src/components/home/Do.tsx
+++ src/components/home/Do.tsx
@@ -1,33 +1,30 @@
-
 import Image from "next/image";
-import border from "@/public/border.svg"
-
+import border from "@/public/border.svg";
 
 const Do = () => {
-  return( 
-  <div className="w-full flex-row rounded-lg bg-hsa-tan-100">
-      <div className = "w-3/4 h-3/4 flex-row p-8 space-y-5  ">
-        <div className = "w-full text-4xl font-bold font-songMyung  text-hsa-blue-100">What We Do</div>
-        <div className="w-full h-3/4
-         text-md font-semibold text-hsa-gray-300 font-open-sans">
-        <p>Our team is made up of a group of talented individuals who are passionate about technology and innovation.</p>
+  return (
+    <div className="w-full flex-row rounded-lg bg-hsa-tan-100">
+      <div className="h-3/4 w-3/4 flex-row space-y-5 p-8">
+        <div className="font-songMyung w-full text-4xl font-bold text-hsa-blue-100">
+          What We Do
+        </div>
+        <div className="text-md font-open-sans h-3/4 w-full font-semibold text-hsa-gray-300">
+          <p>
+            Our team is made up of a group of talented individuals who are
+            passionate about technology and innovation.
+          </p>
         </div>
       </div>
-      <div className="w-full box-content-1 space-y-5">
-      <Image src={border} alt="add image description here" className = "w-full object-contain"/>
+      <div className="box-content-1 w-full space-y-5">
+        <Image
+          src={border}
+          alt="add image description here"
+          className="w-full object-contain"
+        />
 
-      <div className="w-full h-16 bg-hsa-gray-200"></div>
+        <div className="h-16 w-full bg-hsa-gray-200"></div>
       </div>
-    
-  </div>
-    
-    
-    
-    
-    
-    
-    
-    
+    </div>
   );
 };
 
