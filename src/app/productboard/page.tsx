cdd8e3cdacecada070fcc31758254b1de6057d3c
fardinzam <fardinzaman51@gmail.com>
2025-01-28 20:28:20 -0800

    formatted code

--- src/app/productboard/page.tsx
+++ src/app/productboard/page.tsx
@@ -1,26 +1,24 @@
-import Footer from "@/components/Footer"
+import Footer from "@/components/Footer";
 
-const Page = () => {    
-    return (
-        <>
-            <div className="bg-hsa-pink-400 flex flex-row justify-around rounded-2xl">
-                <div className="bg-purple-200 flex flex-row gap-5">
-                    <div className="bg-gray-300 h-12 w-12" />
-                    <p>productboard</p>
-                </div>
-                <div className="bg-blue-500 flex flex-row gap-5">
-                    <p>features</p>
-                    <p>pricing</p>
-                    <p>blog</p>
-                    <p>about</p>
-                </div>
+const Page = () => {
+  return (
+    <>
+      <div className="flex flex-row justify-around rounded-2xl bg-hsa-pink-400">
+        <div className="flex flex-row gap-5 bg-purple-200">
+          <div className="h-12 w-12 bg-gray-300" />
+          <p>productboard</p>
+        </div>
+        <div className="flex flex-row gap-5 bg-blue-500">
+          <p>features</p>
+          <p>pricing</p>
+          <p>blog</p>
+          <p>about</p>
+        </div>
+      </div>
+      <div className="bg-red-400">textbox</div>
+      <div className="bg-green-200">picture of guy</div>
+    </>
+  );
+};
 
-            </div>
-            <div className="bg-red-400">textbox</div>
-            <div className="bg-green-200">picture of guy</div>
-        </>
-    );
-  };
-  
 export default Page;
-  
