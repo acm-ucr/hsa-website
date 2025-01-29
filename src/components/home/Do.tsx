d3fe503b6801b748baa6364396e2a30e823652db
tanishqmano <tanishqmanohar@gmail.com>
2025-01-28 19:22:39 -0800

    Added text

--- src/components/home/Do.tsx
+++ src/components/home/Do.tsx
@@ -6,18 +6,13 @@ const Do = () => {
   return (
     <div className="bg-hsatan-100 w-full flex-row rounded-lg">
       <div className="ml-5 h-3/4 w-3/4 space-y-5 p-10">
-        <div className="font-Semibold w-full font-songMyung text-6xl text-hsa-blue-100">
-          What We Do
-        </div>
+        <div className="font-songMyung text-6xl text-hsa-blue-100">What We Do</div>
         <div className="text-2x1 font-open-sans font-semibold text-hsa-gray-300">
-          <p>
-            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-            wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
-          </p>
+          We are the Hmong Student Association (HSA) at the University of
+          California, Riverside (UCR), and we need your support! As an
+          organization committed to empowering and uniting Hmong students on
+          campus, we strive to create an inclusive, educational, and culturally
+          enriching space for students to learn, grow, and connect.
         </div>
       </div>
       <div className="w-full space-y-5">
