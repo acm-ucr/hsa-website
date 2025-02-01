67827e12218fb1cae5741ef5826eafd86560481e
shuuvi <janeleebmes@gmail.com>
2025-02-01 01:47:59 -0800

    fixed the next/image, add differnt prop components to page.tsx, badge positioning, added board profile webp

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,5 +1,13 @@
+import HeroCard from "./HeroCard";
+import JoinButton from "./JoinButton";
 const Hero = () => {
-  return <div>Hero Section</div>;
+  return (
+    <div className="">
+      <HeroCard />
+      <JoinButton />
+      <div className="mb-5 max-w-full bg-hsa-yellow-100 p-4 shadow-md"></div>
+    </div>
+  );
 };
 
 export default Hero;
--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -1,23 +1,27 @@
 import hsa4 from "@/public/assets/hsa4.svg";
+import Image from "next/image";
 
 const Mission = () => {
   return (
-    <div className="flex w-full justify-between">
-      <div className="w-3/4 flex-col p-10">
-        <div className="ml-5 p-5 text-left font-serif text-6xl text-blue-800">
-          Our Mission
+    <>
+      <div className="flex w-full justify-between">
+        <div className="w-3/4 flex-col p-10">
+          <div className="ml-5 p-5 text-left font-serif text-6xl text-blue-800">
+            Our Mission
+          </div>
+          <div className="ml-5 p-5 text-left text-2xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </div>
         </div>
-        <div className="ml-5 p-5 text-left text-2xl">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
+        <div className="relative left-6 flex justify-end">
+          <Image src={hsa4} alt="HSA" className="w-3/4" />
         </div>
       </div>
-      <div className="relative left-6 flex justify-end">
-        <img src={hsa4.src} alt="HSA" className="w-3/4" />
-      </div>
-    </div>
+      <div className="mb-5 max-w-full bg-hsa-green-100 p-4 shadow-md"></div>
+    </>
   );
 };
 
