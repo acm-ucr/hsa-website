8d07c850918cf4bb4622bd8c9964624071d89bd5
Juan Aguilar <michelleaguilar765@gmail.com>
2025-02-02 01:29:53 -0800

    formatted the 404 Page and fixed the issues of the layout

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,31 +1,40 @@
 import Image from "next/image";
 import gallery1 from "@/public/assets/gallery1/Group 22.png";
 import gallery2 from "@/public/assets/gallery2/Group 23.png";
+import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="grid h-full w-full flex-auto columns-3 grid-flow-col">
-      <div className="pt-8">
-        <Image src={gallery1} width={300} height={300} alt="Gallery 1" />
+    <div className="grid flex-grow grid-cols-3">
+      <div className="flex items-start justify-start">
+        <Image
+          src={gallery1}
+          alt="Gallery 2"
+          className="mt-[10%] object-scale-down"
+        />
       </div>
 
-      <div className="m-8 w-96 justify-center pt-20 text-center">
-        <div className="flex-auto font-songMyung text-9xl font-semibold text-hsa-blue-100">
+      <div className="flex flex-col items-center justify-center">
+        <p className="font-songMyung text-9xl font-semibold text-hsa-blue-100">
           404
-        </div>
-
-        <div className="font-lightweight pt-3 font-sans text-xl text-hsa-gray-300">
+        </p>
+        <p className="font-lightweight pt-3 font-sans text-xl text-hsa-gray-300">
           page not found!
-        </div>
-        <div className="flex-auto pt-4">
-          <button className="rounded-full bg-hsa-green-100 px-12 py-3 font-sans font-bold text-white hover:bg-hsa-green-100">
-            home
-          </button>
-        </div>
+        </p>
+        <Link
+          href="/"
+          className="mt-[5%] rounded-full bg-hsa-green-100 px-12 py-3 font-sans font-bold text-white hover:bg-hsa-green-100"
+        >
+          home
+        </Link>
       </div>
 
-      <div className="pt-40">
-        <Image src={gallery2} width={300} height={300} alt="Gallery 2" />
+      <div className="flex items-end justify-end">
+        <Image
+          src={gallery2}
+          alt="Gallery 2"
+          className="mb-[10%] object-scale-down"
+        />
       </div>
     </div>
   );
