59703ca6d144757ea4ef1c1eab49c27710c7aa7a
daniyalbhat34 <daniyalbhat34@gmail.com>
2025-02-04 11:30:23 -0800

    Added the svg image and logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,34 +1,44 @@
-import Border from "./Border";
+import Border from "@/public/assets/border.svg";
 import Image from "next/image";
-import Facebook from "@/public/assets/Facebook.png";
-import Instagram from "@/public/assets/insta.png";
-import Image2 from "@/public/assets/image2.png";
-import Linktree from "@/public/assets/Linktree.png";
-import Contact from "@/public/assets/contact.png";
+import { Facebook } from "lucide-react";
+import { Instagram } from "lucide-react";
+import Image2 from "@/public/assets/image2.svg";
+import Linktree from "@/public/assets/Linktree.svg";
 import Link from "next/link";
 
 const Footer = () => {
   return (
     <div>
-      <Border />
-      <div className="bg-hsa-gray-200 pb-6 pt-6">
-        <div className="sm:px-15 flex flex-row justify-between px-8">
-          <div className="flex flex-row justify-start">
+      <Image src={Border} alt="border" className="pb-10 pr-8" />
+      <div className="bg-hsa-gray-200 py-6">
+        <div className="sm:px-15 flex justify-between px-8">
+          <div className="flex justify-start">
             <Image src={Image2} alt="Image2" className="w-10 sm:w-14" />
           </div>
-          <div className="flex w-[50%] flex-row justify-end gap-3 pb-1 pt-2">
-            <Image src={Contact} alt="Contact" className="pb-1" />
-            <Link href="https://www.instagram.com/hsa.ucr/" target="_blank">
-              <Image src={Instagram} alt="Instagram" className="pt-1" />
+          <div className="flex w-[50%] justify-end gap-3 pb-1 pt-3 font-semibold text-white">
+            <div className="pb-4 text-xl duration-300 hover:scale-110">
+              Contact us!
+            </div>
+            <Link
+              href="https://www.instagram.com/hsa.ucr/"
+              target="_blank"
+              className="duration-300 hover:scale-110"
+            >
+              <Instagram />
             </Link>
             <Link
               href="https://www.facebook.com/hsa.ucriverside"
               target="_blank"
+              className="duration-300 hover:scale-110"
             >
-              <Image src={Facebook} alt="Facebook" />
+              <Facebook />
             </Link>
-            <Link href="linktr.ee/hsaucr" target="_blank">
-              <Image src={Linktree} alt="Facebook" />
+            <Link href="linktr.ee/hsaucr" target="_blank" className="w-5">
+              <Image
+                src={Linktree}
+                alt="Facebook"
+                className="duration-300 hover:scale-110"
+              />
             </Link>
           </div>
         </div>
