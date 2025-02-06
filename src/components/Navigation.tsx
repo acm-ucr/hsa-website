138ca18650330097b305cae16a3e5761409fafee
Juan Aguilar <michelleaguilar765@gmail.com>
2025-02-05 20:32:52 -0800

    fixed the navigation bar and made the text a bit smaller like asked

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+import hsaImage from "@/public/hsaImage.webp";
 
 const Navigation = () => {
   return (
@@ -6,28 +8,28 @@ const Navigation = () => {
       <div className="mx-auto flex py-8 text-white">
         <div className="flex items-center">
           <Link href="/">
-            <img
-              src="/hsaImage.webp"
+            <Image
+              src={hsaImage}
               alt="logo"
-              className="ml-12 mr-8 h-20 w-20"
+              className="ml-12 mr-10 object-scale-down"
             />
           </Link>
-          <div className="text-6xl"> HSA</div>
+          <div className="ml-[30%] text-5xl"> HSA</div>
         </div>
-        <ul className="ml-60 flex items-center space-x-36 text-4xl">
+        <ul className="ml-[40%] flex w-2/3 items-center space-x-10 text-2xl">
           <li className="hover:text-hsa-pink-500">
-            <Link href="/home">Home</Link>
+            <Link href="/">Home</Link>
           </li>
-          <li className="hover:text-hsa-pink-500">
+          <li className="hover:text-hsa-pink-500 pl-[8%]">
             <Link href="/board">Board</Link>
           </li>
-          <li className="hover:text-hsa-pink-500">
+          <li className="hover:text-hsa-pink-500 pl-[8%]">
             <Link href="/events">Events</Link>
           </li>
-          <li className="hover:text-hsa-pink-500">
+          <li className="hover:text-hsa-pink-500 pl-[8%]">
             <Link href="/calendar">Calendar</Link>
           </li>
-          <li className="hover:text-hsa-pink-500">
+          <li className="hover:text-hsa-pink-500 pl-[8%]">
             <Link href="/gallery">Gallery</Link>
           </li>
         </ul>
