c7ebc65dabf12f9e2d48418e104861a63b8a73a7
cids <sidc646@gmail.com>
2025-02-07 17:14:41 -0800

    format

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,5 +1,24 @@
 const About = () => {
-  return <div>About Us</div>;
+  return (
+    <div className="flex bg-white p-10">
+      <div className="w-3/5"></div>
+      <div className="w-2/5 text-right">
+        <h1 className="ml-5 p-5 font-serif text-6xl text-blue-800">About Us</h1>
+        <p className="text-lg text-gray-800">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p className="text-lg text-gray-800">
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
