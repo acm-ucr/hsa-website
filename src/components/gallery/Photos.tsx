ffc65778e16bd7fd3addff2962c5c1ba58a6ee15
tanishqmano <tanishqmanohar@gmail.com>
2025-02-11 20:10:41 -0800

    Made new changes

--- src/components/gallery/Photos.tsx
+++ src/components/gallery/Photos.tsx
@@ -1,23 +1,22 @@
 "use client";
 
 import React from "react";
-import { Gallery } from "react-grid-gallery";
 
 const images_gallery = [
-  { src: "/gallery images/image1.webp",},
-  { src: "/gallery images/image2.webp", },
-  { src: "/gallery images/image3.webp", },
-  { src: "/gallery images/image4.webp", },
-  { src: "/gallery images/image6.webp",  },
-  { src: "/gallery images/image7.webp", },
-  { src: "/gallery images/image8.webp", },
-  { src: "/gallery images/image9.webp", },
-  { src: "/gallery images/image10.webp", },
-  { src: "/gallery images/image11.webp", },
-  { src: "/gallery images/image12.webp", },
-  { src: "/gallery images/image13.webp", },
-  { src: "/gallery images/image14.webp", },
-  { src: "/gallery images/image15.webp", },
+  { src: "/gallery images/image1.webp" },
+  { src: "/gallery images/image2.webp" },
+  { src: "/gallery images/image3.webp" },
+  { src: "/gallery images/image4.webp" },
+  { src: "/gallery images/image6.webp" },
+  { src: "/gallery images/image7.webp" },
+  { src: "/gallery images/image8.webp" },
+  { src: "/gallery images/image9.webp" },
+  { src: "/gallery images/image10.webp" },
+  { src: "/gallery images/image11.webp" },
+  { src: "/gallery images/image12.webp" },
+  { src: "/gallery images/image13.webp" },
+  { src: "/gallery images/image14.webp" },
+  { src: "/gallery images/image15.webp" },
 ];
 
 const Photos = () => {
@@ -25,12 +24,14 @@ const Photos = () => {
     <div className="m-1/10 grid grid-cols-3 gap-4 p-4">
       {images_gallery.map((image, index) => (
         <div key={index} className="overflow-hidden rounded-lg shadow-md">
-          <img src={image.src} alt="gallery" className="w-full h-full object-cover" />
-          </div>
+          <img
+            src={image.src}
+            alt="gallery"
+            className="h-full w-full object-cover"
+          />
+        </div>
       ))}
-
-          
-    </div> 
+    </div>
   );
 };
 
