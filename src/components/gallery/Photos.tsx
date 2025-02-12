22f5975e9d80d48ab60e3e9d7697f7884fa969f5
tanishqmano <tanishqmanohar@gmail.com>
2025-02-11 19:58:38 -0800

    Finally did it

--- src/components/gallery/Photos.tsx
+++ src/components/gallery/Photos.tsx
@@ -1,5 +1,37 @@
+"use client";
+
+import React from "react";
+import { Gallery } from "react-grid-gallery";
+
+const images_gallery = [
+  { src: "/gallery images/image1.webp",},
+  { src: "/gallery images/image2.webp", },
+  { src: "/gallery images/image3.webp", },
+  { src: "/gallery images/image4.webp", },
+  { src: "/gallery images/image6.webp",  },
+  { src: "/gallery images/image7.webp", },
+  { src: "/gallery images/image8.webp", },
+  { src: "/gallery images/image9.webp", },
+  { src: "/gallery images/image10.webp", },
+  { src: "/gallery images/image11.webp", },
+  { src: "/gallery images/image12.webp", },
+  { src: "/gallery images/image13.webp", },
+  { src: "/gallery images/image14.webp", },
+  { src: "/gallery images/image15.webp", },
+];
+
 const Photos = () => {
-  return <div>Photos</div>;
+  return (
+    <div className="m-1/10 grid grid-cols-3 gap-4 p-4">
+      {images_gallery.map((image, index) => (
+        <div key={index} className="overflow-hidden rounded-lg shadow-md">
+          <img src={image.src} alt="gallery" className="w-full h-full object-cover" />
+          </div>
+      ))}
+
+          
+    </div> 
+  );
 };
 
 export default Photos;
