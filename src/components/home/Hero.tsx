960b63134616cd05059a325f7190134e32061af5
Arik Lee <ariklee@ucr-secure-25-10-13-150-52.wnet.ucr.edu>
2025-02-12 21:44:08 -0800

    Hero Image Fixed

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,11 +1,26 @@
 import HeroCard from "./HeroCard";
 import JoinButton from "./JoinButton";
+import Image from "next/image";
+import hero from "@/public/assets/hero.svg";
+
 const Hero = () => {
   return (
     <div className="">
+      <div className="relative mx-auto w-2/5">
+        <Image
+          className="absolute -bottom-56 -left-36 z-10 transform"
+          src={hero}
+          alt="hero border component"
+        />
+        <Image
+          className="absolute -right-36 top-8 z-10 scale-x-[-1] scale-y-[-1] transform"
+          src={hero}
+          alt="hero border component"
+        />
+      </div>
       <HeroCard />
       <JoinButton />
-      <div className="h-8 max-w-full bg-hsa-yellow-100 shadow-md"></div>
+      <div className="mb-5 max-w-full bg-hsa-yellow-100 p-4 shadow-md"></div>
     </div>
   );
 };
