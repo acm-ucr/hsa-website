95e8b8392b4d7a5343038c753cc3a8420a0a81a1
Ayush <68819610+akothule@users.noreply.github.com>
2025-02-13 19:03:47 -0800

    Fixed Image Offset

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,15 +9,15 @@ const Header = ({ title }: props) => {
   return (
     <div className="flex flex-row justify-between bg-hsa-gray-100 drop-shadow-2xl">
       <div className="flex flex-row justify-center overflow-hidden">
-        <Image src={hsa5} alt="HSA5" className="relative top-16 w-3/4" />
+        <Image src={hsa5} alt="HSA5" className="relative top-1/3 w-3/4" />
       </div>
       <div className="flex flex-col justify-center">
-        <div className="w-full pr-10 font-songMyung text-2xl text-gray-700 md:text-5xl lg:text-7xl">
+        <div className="w-full pr-10 font-songMyung text-3xl text-gray-700 md:text-5xl lg:text-7xl">
           {title}
         </div>
       </div>
       <div className="flex flex-row justify-center overflow-hidden">
-        <Image src={hsa5} alt="HSA5" className="relative bottom-16 w-3/4" />
+        <Image src={hsa5} alt="HSA5" className="relative bottom-1/3 w-3/4" />
       </div>
     </div>
   );
