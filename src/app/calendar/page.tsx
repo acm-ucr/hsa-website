acc5a15ba20e4ae8a50f80ab0eca97d0eaa0a7f5
unknown <twu148@ucr.edu>
2025-02-17 19:38:40 -0800

    event cards (calendar)

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,13 +1,20 @@
 import CalendarEvents from "@/components/calendar/CalendarEvents";
 import EventCard from "@/components/calendar/EventCard";
 import Header from "@/components/Header";
+import eventCardData from "@/data/EventCardData";
 
 const Page = () => {
   return (
     <>
-      <Header title="CALENDAR" />
-      <EventCard />
-      <CalendarEvents />
+      <Header title="Calendar" />
+      <div className="my-6">
+        {eventCardData.map((event, index) => (
+          <EventCard key={index} data={event} />
+        ))}
+      </div>
+      <div className="bg-hsa-tan-200 mb-8">
+        <CalendarEvents />
+      </div>
     </>
   );
 };
--- src/components/calendar/EventCard.tsx
+++ src/components/calendar/EventCard.tsx
@@ -1,37 +1,41 @@
 import hsa7 from "@/public/assets/hsa7.svg";
 import Image from "next/image";
 
-const MOCKDATA = {
-  name: "Event Name",
-  location: "SSC MPR 2",
-  date: "NOV 25",
-  time: "7:00 PM",
-  description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia earum itaque quasi ea dolores voluptatum. Debitis sint voluptatibus ab consectetur quam, unde dolorum consequatur laborum numquam atque totam fuga quaerat!"
+interface EventData {
+  name: string;
+  location: string;
+  date: string;
+  time: string;
+  description: string;
 }
 
-const EventCard = () => {
+interface EventCardProps {
+  data: EventData;
+}
+
+const EventCard: React.FC<EventCardProps> = ({ data }) => {
   return (
-    <div className="bg-white m-6 p-6 w-3/4 flex flex-row gap-4 rounded-2xl shadow-md shadow-gray-500">
-      <div className="flex flex-col text-center font-songMyung justify-center align-middle">
+    <div className="mx-auto bg-white text-hsa-gray-300 m-6 p-6 w-2/3 flex flex-row gap-6 rounded-3xl shadow-md shadow-gray-500">
+      <div className="flex flex-col text-center font-songMyung justify-center align-middle p-4">
         <Image src={hsa7} width={300} alt="Decoration"/>
-        <div className="text-4xl/8 mt-4 w-full px-4">
-          {MOCKDATA.date}
+        <div className="text-5xl/10 mt-4 w-full px-4">
+          {data.date}
         </div>
-        <div className="text-2xl mb-4">
-          {MOCKDATA.time}
+        <div className="text-4xl mb-4">
+          {data.time}
         </div>
         <Image className="rotate-180" src={hsa7} width={300} alt="Decoration"/>
       </div>
-      <div className="bg-hsa-pink-400 w-2 rounded-xl"/>
-      <div className="flex flex-col my-auto">
-        <div className="text-3xl font-songMyung">
-          {MOCKDATA.name}
+      <div className="bg-hsa-pink-400 w-6 rounded-xl"/>
+      <div className="flex flex-col gap-2 my-auto p-4">
+        <div className="text-5xl font-songMyung">
+          {data.name}
         </div>
-        <div>
-          Location: {MOCKDATA.location}
+        <div className="text-xl font-semibold">
+          Location: {data.location}
         </div>
-        <div>
-          {MOCKDATA.description}
+        <div className="text-xl p-2 pl-0">
+          {data.description}
         </div>
       </div>
     </div>
