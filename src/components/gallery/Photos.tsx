794476beef71cadbcd386ba28c30ce70d926f623
Ayush <68819610+akothule@users.noreply.github.com>
2025-02-25 12:12:05 -0800

    akothule/GalleryResponsiveness

--- src/components/gallery/Photos.tsx
+++ src/components/gallery/Photos.tsx
@@ -1,33 +1,36 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 
 const images_gallery = [
-  { src: "/gallery images/image1.webp" },
-  { src: "/gallery images/image2.webp" },
-  { src: "/gallery images/image3.webp" },
-  { src: "/gallery images/image4.webp" },
-  { src: "/gallery images/image6.webp" },
-  { src: "/gallery images/image7.webp" },
-  { src: "/gallery images/image8.webp" },
-  { src: "/gallery images/image9.webp" },
-  { src: "/gallery images/image10.webp" },
-  { src: "/gallery images/image11.webp" },
-  { src: "/gallery images/image12.webp" },
-  { src: "/gallery images/image13.webp" },
-  { src: "/gallery images/image14.webp" },
-  { src: "/gallery images/image15.webp" },
+  { src: "/gallery/image1.webp" },
+  { src: "/gallery/image2.webp" },
+  { src: "/gallery/image3.webp" },
+  { src: "/gallery/image4.webp" },
+  { src: "/gallery/image6.webp" },
+  { src: "/gallery/image7.webp" },
+  { src: "/gallery/image8.webp" },
+  { src: "/gallery/image9.webp" },
+  { src: "/gallery/image10.webp" },
+  { src: "/gallery/image11.webp" },
+  { src: "/gallery/image12.webp" },
+  { src: "/gallery/image13.webp" },
+  { src: "/gallery/image14.webp" },
+  { src: "/gallery/image15.webp" },
 ];
 
 const Photos = () => {
   return (
-    <div className="m-1/10 grid grid-cols-3 gap-4 p-4">
+    <div className="m-1/10 grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3">
       {images_gallery.map((image, index) => (
         <div key={index} className="overflow-hidden rounded-lg shadow-md">
-          <img
+          <Image
             src={image.src}
             alt="gallery"
             className="h-full w-full object-cover"
+            width={500}
+            height={500}
           />
         </div>
       ))}
