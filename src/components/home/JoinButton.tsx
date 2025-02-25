2c7d472cc1d4abb83d43979859a3a1a5d4ab59a8
Ayush <68819610+akothule@users.noreply.github.com>
2025-02-25 10:07:00 -0800

    Added Hover Design to Join Us Button

--- src/components/home/JoinButton.tsx
+++ src/components/home/JoinButton.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 const JoinButton = () => {
   return (
-    <div className="mx-auto mb-12 max-w-44 rounded-full bg-hsa-green-100 pb-3 pt-2 text-center font-openSans text-2xl font-semibold text-hsa-tan-100">
+    <div className="mx-auto mb-12 max-w-44 rounded-full bg-hsa-green-100 pb-3 pt-2 text-center font-openSans text-2xl font-semibold text-hsa-tan-100 hover:bg-green-900 hover:font-bold hover:text-hsa-tan-200 hover:drop-shadow-lg">
       <Link href="https://discord.gg/BMx92Z2bsY">Join us!</Link>
     </div>
   );
