720b1a6a2a0aaede7bb381e628fca61dfb7665a9
shuuvi <janeleebmes@gmail.com>
2025-02-26 00:10:10 -0800

    created footer map, deleted linktree img, fixed links, and mobile resposnes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,28 +3,32 @@ import Link from "next/link";
 import Image from "next/image";
 import Border from "@/public/assets/border.svg";
 import footerImage from "@/public/assets/HSAlogo.svg";
-import {Facebook} from "lucide-react";
+import { Facebook } from "lucide-react";
 
 const Footer = () => {
   return (
     <div>
       <Image src={Border} alt="border" className="pb-10 pr-8" />
-      <div className=" flex items-center justify-between bg-hsa-gray-200">
-          <Image src={footerImage} alt="Image2" className="flex w-full justify-items-start ml-8 sm:w-14 bg-red-300"/>
+      <div className="flex items-center justify-between bg-hsa-gray-200 p-3">
+        <Image
+          src={footerImage}
+          alt="Image2"
+          className="ml-4 flex w-1/6 justify-items-start p-1 md:w-14"
+        />
 
-          <div className="flex w-1/4 font-semibold text-white bg-slate-600">
-            <div className="pb-4 text-xl duration-300">Contact us!</div>
-            {footers.map((link, index) => (
-              <Link
-              key = {index}
-              href= {link.link}  
+        <div className="flex place-content-center font-openSans text-xl font-semibold text-white md:w-1/6">
+          <p className="mr-3">Contact Us!</p>
+          {footers.map((link, index) => (
+            <Link
+              key={index}
+              href={link.link}
               target="_blank"
-              className="duration-300 hover:scale-110"
+              className="mr-2 flex duration-300 hover:scale-110"
             >
               {link.icon}
             </Link>
-            ))}
-          </div>
+          ))}
+        </div>
       </div>
     </div>
   );
