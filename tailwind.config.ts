78f4fe0bd2c720cd1442c0bf6ab26752a9d7897d
Cole <yoshi88639@gmail.com>
2025-02-26 16:49:07 -0800

    initial commit, shadcn calendar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,80 +1,76 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: ["./src/**/*.{ts,tsx}"],
+  darkMode: ["class"],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-  	extend: {
-  		colors: {
-  			hsa: {
-  				'tan-100': '#FBF3F0',
-  				'tan-200': '#F4DCD2',
-  				'green-100': '#3B6C46',
-  				'red-100': '#B31A25',
-  				'pink-100': '#FFF0F0',
-  				'pink-200': '#FD5B67',
-  				'pink-300': '#B31A25',
-  				'pink-400': '#FCA9AC',
-  				'yellow-100': '#E0BC71',
-  				'blue-100': '#1B3BA4',
-  				'gray-100': '#EFEFEF',
-  				'gray-200': '#AFAFAF',
-  				'gray-300': '#424242'
-  			},
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		fontFamily: {
-  			songMyung: [
-  				'var(--song-myung-font)'
-  			],
-  			openSans: [
-  				'var(--open-sans-font)'
-  			]
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        hsa: {
+          "tan-100": "#FBF3F0",
+          "tan-200": "#F4DCD2",
+          "green-100": "#3B6C46",
+          "red-100": "#B31A25",
+          "pink-100": "#FFF0F0",
+          "pink-200": "#FD5B67",
+          "pink-300": "#B31A25",
+          "pink-400": "#FCA9AC",
+          "yellow-100": "#E0BC71",
+          "blue-100": "#1B3BA4",
+          "gray-100": "#EFEFEF",
+          "gray-200": "#AFAFAF",
+          "gray-300": "#424242",
+        },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+      },
+      fontFamily: {
+        songMyung: ["var(--song-myung-font)"],
+        openSans: ["var(--open-sans-font)"],
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
