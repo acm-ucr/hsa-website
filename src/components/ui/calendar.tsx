1fcfdb5bc2fab939869133aaa4578b4296d65f31
Cole <yoshi88639@gmail.com>
2025-03-03 01:25:41 -0800

    Gcal API integration

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import * as React from "react";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { MoveLeft, MoveRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "./button";
 
 export type GoogleEventProps = {
   start: {
@@ -32,62 +32,113 @@ export type CalendarProps = React.ComponentProps<typeof DayPicker> & {
   setCurrent: (props: EventProps) => void;
 };
 
+interface DayProps {
+  date: Date;
+  displayMonth: Date;
+  events: EventProps[];
+  setCurrent: (props: EventProps) => void;
+}
+
+const Day = ({ date, displayMonth, events, setCurrent }: DayProps) => {
+  const currentMonth = displayMonth.getMonth() === date.getMonth();
+
+  return (
+    <div
+      className={`${currentMonth ? "text-hsa-gray-300" : "text-hsa-gray-200"} scrollbar-hidden h-24 overflow-y-scroll border`}
+    >
+      <p className="sticky px-2 text-right">{date.getDate()}</p>
+
+      {events?.map(({ title, start, end, location, description }, index) => {
+        const startDate = new Date(start as string);
+
+        if (
+          startDate.getDate() === date.getDate() &&
+          startDate.getMonth() === date.getMonth() &&
+          startDate.getFullYear() === date.getFullYear()
+        ) {
+          return (
+            <div
+              className="my-1 cursor-pointer text-ellipsis bg-hsa-blue-100 bg-opacity-75 p-1 text-center text-hsa-tan-100 hover:bg-opacity-100"
+              key={index}
+              onClick={() =>
+                setCurrent({ title, start, end, location, description })
+              }
+            >
+              {startDate.getHours() < 12
+                ? (startDate.getHours() % 12) + "am"
+                : (startDate.getHours() % 12) + "pm"}{" "}
+              {title}
+            </div>
+          );
+        }
+      })}
+    </div>
+  );
+};
+
 function Calendar({
   className,
   classNames,
   showOutsideDays = true,
+  events,
+  setCurrent,
   ...props
 }: CalendarProps) {
   return (
     <DayPicker
       showOutsideDays={showOutsideDays}
-      className={cn("p-3", className)}
+      className={cn("rounded-none border-none bg-hsa-gray-100 p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
-        month: "space-y-4",
-        caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        month: "space-y-4 w-2/3 flex justify-center flex-col mx-auto",
+        caption: "flex justify-center p-1 relative items-center",
+        caption_label: "text-md font-bold text-hsa-gray-300",
         nav: "space-x-1 flex items-center",
-        nav_button: cn(
-          buttonVariants({ variant: "outline" }),
-          "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
-        ),
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
+        nav_button: "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
+        nav_button_previous: "absolute left-1/3",
+        nav_button_next: "absolute right-1/3",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md w-8 font-normal text-[0.8rem]",
-        row: "flex w-full mt-2",
-        cell: cn(
-          "relative p-0 text-center text-sm focus-within:relative focus-within:z-20 [&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
-          props.mode === "range"
-            ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
-            : "[&:has([aria-selected])]:rounded-md",
-        ),
+          "text-muted-foreground w-full font-normal text-[0.8rem] border text-[#EFEFEF] bg-hsa-pink-200 bg-opacity-100", //this is so scuffed but for some reason text-hsa-gray-100 doesnt work
+        row: "flex w-full",
+        cell: "w-full text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
-          "h-8 w-8 p-0 font-normal aria-selected:opacity-100",
+          "w-full p-0 font-normal aria-selected:opacity-100 rounded-none border",
         ),
-        day_range_start: "day-range-start",
         day_range_end: "day-range-end",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground border-none",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
           "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
-        day_disabled: "text-muted-foreground opacity-50",
+        day_disabled: "text-hsa-gray-300 opacity-50",
         day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "aria-selected:bg-accent aria-selected:text-hsa-gray-300",
         day_hidden: "invisible",
         ...classNames,
       }}
       components={{
         IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
+          <MoveLeft
+            className={cn("h-5 w-5 text-hsa-gray-300", className)}
+            {...props}
+          />
         ),
         IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
+          <MoveRight
+            className={cn("h-5 w-5 text-hsa-gray-300", className)}
+            {...props}
+          />
+        ),
+        Day: ({ displayMonth, date }) => (
+          <Day
+            date={date}
+            displayMonth={displayMonth}
+            events={events}
+            setCurrent={setCurrent}
+          />
         ),
       }}
       {...props}
