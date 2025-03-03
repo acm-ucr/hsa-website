de6a6d4202d804d2c235c0c6d9071624b3e40ecb
Juan Aguilar <michelleaguilar765@gmail.com>
2025-03-03 13:14:13 -0800

    update local

--- src/components/home/JoinButton.tsx
+++ src/components/home/JoinButton.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 const JoinButton = () => {
   return (
+<<<<<<< Updated upstream
     <div className="mx-auto mb-12 max-w-44 rounded-full bg-hsa-green-100 pb-3 pt-2 text-center font-openSans text-2xl font-semibold text-hsa-tan-100 hover:bg-green-900 hover:font-bold hover:text-hsa-tan-200 hover:drop-shadow-lg">
+=======
+    <div className="mx-auto mb-12 max-w-44 rounded-full bg-hsa-green-100 pb-3 pt-2 text-center font-openSans text-2xl font-semibold text-hsa-tan-100">
+>>>>>>> Stashed changes
       <Link href="https://discord.gg/BMx92Z2bsY">Join us!</Link>
     </div>
   );
--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -4,18 +4,19 @@ import Image from "next/image";
 const Mission = () => {
   return (
     <>
+   
       <div className="flex w-full justify-between">
         <div className="my-5 w-3/4 flex-col p-12">
-          <div className="text-left font-songMyung text-5xl text-hsa-blue-100">
+          <div className=" md:text-center text-left font-songMyung text-5xl text-hsa-blue-100">
             Our Mission
           </div>
-          <div className="-ml-4 mr-5 p-5 text-left font-openSans text-xl font-semibold text-hsa-gray-300">
+          <div className="-ml-4 mr-5 p-5 text-left sm:text-center font-openSans text-xl font-semibold text-hsa-gray-300">
             Here at HSA @UCR, our mission is to spread knowledge of our history
             & culture, and to build a safe & uplifting Hmong community
           </div>
         </div>
-        <div className="relative flex">
-          <Image src={hsa4} alt="HSA" className="w-3/4" />
+        <div className="relative flex sm:invisible ">
+          <Image src={hsa4} alt="HSA" className= " w-3/4" />
         </div>
       </div>
       <div className="h-8 max-w-full bg-hsa-green-100 shadow-md"></div>
