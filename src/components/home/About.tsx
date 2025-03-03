ec754799ee38e7c7af727a44fcdc06a317f22f20
Brian Byun <brianubyun@gmail.com>
2025-03-03 01:50:30 -0800

    changed to div

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -69,9 +69,9 @@ const About = () => {
           </div>
         </motion.div>
         <div className="my-5 flex w-full max-w-full flex-col justify-center px-4 text-center md:w-1/2 md:px-24 md:text-right">
-          <h1 className="font-songMyung text-5xl text-hsa-blue-100">
+          <div className="font-songMyung text-5xl text-hsa-blue-100">
             About Us
-          </h1>
+          </div>
           <p className="mt-4 flex font-openSans text-xl font-semibold text-hsa-gray-300">
             HSA @ UCR is one of the few Hmong Student Associations in Southern
             California. We strive to be a "home away from home" for all
