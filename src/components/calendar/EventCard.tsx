3c8c9427a1498fb51646b12ec9f85e51c8a0a57b
unknown <twu148@ucr.edu>
2025-03-03 02:00:15 -0800

    Destructure data prop

--- src/components/calendar/EventCard.tsx
+++ src/components/calendar/EventCard.tsx
@@ -13,20 +13,22 @@ interface EventCardProps {
   data: EventData;
 }
 
-const EventCard: React.FC<EventCardProps> = ({ data }) => {
+const EventCard = ({
+  data: { name, location, date, time, description },
+}: EventCardProps) => {
   return (
     <div className="m-6 mx-auto flex w-2/3 flex-row gap-6 rounded-3xl bg-white p-6 text-hsa-gray-300 shadow-md shadow-gray-500">
       <div className="flex flex-col justify-center p-4 text-center align-middle font-songMyung">
         <Image src={hsa7} width={300} alt="Decoration" />
-        <div className="mt-4 w-full px-4 text-5xl/10">{data.date}</div>
-        <div className="mb-4 text-4xl">{data.time}</div>
+        <div className="mt-4 w-full px-4 text-5xl/10">{date}</div>
+        <div className="mb-4 text-4xl">{time}</div>
         <Image className="rotate-180" src={hsa7} width={300} alt="Decoration" />
       </div>
       <div className="w-6 rounded-xl bg-hsa-pink-400" />
       <div className="my-auto flex flex-col gap-2 p-4">
-        <div className="font-songMyung text-5xl">{data.name}</div>
-        <div className="text-xl font-semibold">Location: {data.location}</div>
-        <div className="p-2 pl-0 text-xl">{data.description}</div>
+        <div className="font-songMyung text-5xl">{name}</div>
+        <div className="text-xl font-semibold">Location: {location}</div>
+        <div className="p-2 pl-0 text-xl">{description}</div>
       </div>
     </div>
   );
