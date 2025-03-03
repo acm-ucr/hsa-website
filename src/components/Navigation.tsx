c6c7cc4ae3ab8696a06acebf32ad711626560bb4
Arik Lee <ariklee@ucr-secure-31-10-13-175-138.wnet.ucr.edu>
2025-03-03 15:34:57 -0800

    mobile response nav done

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,13 +4,42 @@ import Link from "next/link";
 import Image from "next/image";
 import HSAlogo from "@/public/assets/HSAlogo.svg";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const Navigation = () => {
   const pathName = usePathname();
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <nav className="bg-hsa-pink-200 font-serif">
-      <div className="flex justify-between py-4 text-hsa-tan-100">
+      <div className="flex items-center justify-between px-4 py-4 lg:hidden">
+        <Link href="/" className="flex items-center gap-[1vw]">
+          <Image src={HSAlogo} alt="logo" className="w-12" />
+          <div className="text-[3vw] sm:text-[2vw]">HSA</div>
+        </Link>
+        <div className="py-4 text-hsa-gray-100" onClick={toggleMobileMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="h-6 w-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="3"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </div>
+      </div>
+
+      <div className="hidden justify-between py-4 text-hsa-tan-100 lg:flex">
         <Link href="/" className="ml-[4%] flex items-center gap-[1vw]">
           <Image src={HSAlogo} alt="logo" className="w-12" />
           <div className="text-[3vw] sm:text-[2vw]">HSA</div>
@@ -33,6 +62,27 @@ const Navigation = () => {
           ))}
         </div>
       </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`lg:hidden ${isMobileMenuOpen ? "block" : "hidden"} bg-hsa-pink-200 px-6 py-4 text-hsa-tan-100`}
+      >
+        {tags.map(({ link, name }) => (
+          <div key={link} className="py-2">
+            <Link
+              href={link}
+              className={`${
+                pathName === link
+                  ? "underline decoration-[6px] underline-offset-[10px]"
+                  : "hover:text-hsa-blue-200"
+              }`}
+            >
+              {name}
+            </Link>
+          </div>
+        ))}
+      </div>
+
       <div className="h-4 bg-hsa-blue-100" />
     </nav>
   );
