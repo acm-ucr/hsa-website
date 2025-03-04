08b8e4c440f8dd2d73ae1653d02daad3ac09bb3f
Ayush <68819610+akothule@users.noreply.github.com>
2025-03-04 10:42:35 -0800

    Added Animations to Event Card

--- src/components/events/EventCardBase.tsx
+++ src/components/events/EventCardBase.tsx
@@ -1,5 +1,8 @@
 import Image from "next/image";
 
+// React
+import * as motion from "motion/react-client";
+
 interface EventCardBaseProps {
   title: string;
   shortDescription: string;
@@ -8,6 +11,38 @@ interface EventCardBaseProps {
   width: number;
   height: number;
 }
+
+const titleAnimation = {
+  hidden: { opacity: 0.1, y: -25 },
+  show: {
+    opacity: 1,
+    y: 0,
+  },
+};
+const imageAnimation = {
+  hidden: { opacity: 0.1, x: -25 },
+  show: {
+    opacity: 1,
+    x: 0,
+  },
+};
+const shortTextAnimation = {
+  hidden: { opacity: 0.1, x: 25 },
+  show: {
+    opacity: 1,
+    x: 0,
+  },
+};
+const longTextAnimation = {
+  hidden: { opacity: 0.1, y: 25 },
+  show: {
+    opacity: 1,
+    y: 0,
+  },
+};
+const transition = {
+  duration: 0.25,
+};
 
 const EventCardBase: React.FC<EventCardBaseProps> = ({
   title,
@@ -18,22 +53,43 @@ const EventCardBase: React.FC<EventCardBaseProps> = ({
   height,
 }) => {
   return (
-    <div className="h-200 mx-8 my-10 max-w-4xl rounded-2xl bg-white p-10 shadow-lg md:mx-auto">
-      <div className="mb-6 mt-2 text-center font-songMyung text-3xl font-medium text-hsa-blue-100 md:text-6xl">
+    <motion.div
+      className="h-200 mx-8 my-10 max-w-4xl rounded-2xl bg-white p-10 shadow-lg md:mx-auto"
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.35 }}
+    >
+      <motion.div
+        className="mb-6 mt-2 text-center font-songMyung text-3xl font-medium text-hsa-blue-100 md:text-6xl"
+        variants={titleAnimation}
+        transition={{ ...transition, delay: 0.5 }}
+      >
         {title}
-      </div>
+      </motion.div>
       <div className="flex flex-wrap items-center">
-        <div className="flex items-center md:mr-5">
+        <motion.div
+          className="flex items-center md:mr-5"
+          variants={imageAnimation}
+          transition={{ ...transition, delay: 0.5 }}
+        >
           <Image src={imageURL} width={width} height={height} alt="" />
-        </div>
-        <p className="flex-1 pt-2 font-openSans text-base font-medium text-gray-700 md:text-xl">
+        </motion.div>
+        <motion.p
+          className="flex-1 pt-2 font-openSans text-base font-medium text-gray-700 md:text-xl"
+          variants={shortTextAnimation}
+          transition={{ ...transition, delay: 0.5 }}
+        >
           {shortDescription}
-        </p>
+        </motion.p>
       </div>
-      <p className="mt-6 font-openSans text-base font-medium text-gray-700 md:ml-2 md:mr-16 md:text-xl">
+      <motion.p
+        className="mt-6 font-openSans text-base font-medium text-gray-700 md:ml-2 md:mr-16 md:text-xl"
+        variants={longTextAnimation}
+        transition={{ ...transition, delay: 0.5 }}
+      >
         {longDescription}
-      </p>
-    </div>
+      </motion.p>
+    </motion.div>
   );
 };
 
