7792cdb3533f6b8effdd61eec3ef4c218500fe89
Paul Ingram <ingrampaul411@gmail.com>
2025-03-04 10:26:52 -0800

    Fix animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,14 +6,9 @@ type Props = {
   title: string;
 };
 
-const fadeInFromTop = {
-  hidden: { opacity: 0, y: -50 },
-  visible: { opacity: 1, y: 0, transition: { duration: 1 } },
-};
-
-const fadeInFromBottom = {
-  hidden: { opacity: 0, y: 50 },
-  visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+const fadeIn = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.75 } },
 };
 
 const Header = ({ title }: Props) => {
@@ -23,7 +18,7 @@ const Header = ({ title }: Props) => {
         className="relative z-0 flex flex-row justify-center overflow-hidden"
         initial="hidden"
         animate="visible"
-        variants={fadeInFromBottom}
+        variants={fadeIn}
       >
         <Image src={hsa5} alt="HSA5" className="relative top-1/3 w-2/3" />
       </motion.div>
@@ -32,7 +27,7 @@ const Header = ({ title }: Props) => {
         className="flex flex-col justify-center"
         initial="hidden"
         animate="visible"
-        variants={fadeInFromTop}
+        variants={fadeIn}
         transition={{ delay: 0.3 }}
       >
         <div className="w-full whitespace-nowrap font-songMyung text-3xl text-hsa-gray-300 md:text-6xl">
@@ -44,7 +39,7 @@ const Header = ({ title }: Props) => {
         className="flex flex-row justify-center overflow-hidden"
         initial="hidden"
         animate="visible"
-        variants={fadeInFromBottom}
+        variants={fadeIn}
         transition={{ delay: 0.6 }}
       >
         <Image src={hsa5} alt="HSA5" className="relative bottom-1/3 w-2/3" />
