7ac7f2c3a5d905da9395be85bccaf7d4be59d3ce
shuuvi <janeleebmes@gmail.com>
2025-03-09 03:27:13 -0700

    fixed moble responsiveness AND moved border component to border.tsx

--- src/components/Border.tsx
+++ src/components/Border.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import border from "@/public/assets/border.svg";
+
 const Border = () => {
-  return <div>Border</div>;
+  return <div className="mb-5 w-full overflow-hidden">
+  <div className="flex w-full justify-center gap-x-2">
+    {[...Array(2)].map((_, index) => (
+      <Image
+        key={index}
+        src={border}
+        alt="border"
+        className="w-[70%] min-w-[60]"
+      />
+    ))}
+  </div>
+</div>;
 };
 
 export default Border;
