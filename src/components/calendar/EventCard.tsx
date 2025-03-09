6e976f0b9e543c8e4ce93519aa9b5cab8fa1e07a
Cole <yoshi88639@gmail.com>
2025-03-09 00:18:07 -0800

    Responsify Event Card, changed default look to be a bit more full

--- src/components/calendar/EventCard.tsx
+++ src/components/calendar/EventCard.tsx
@@ -12,18 +12,32 @@ const EventCard = ({ title, location, start, description }: EventProps) => {
     minute: "2-digit",
   });
   return (
-    <div className="m-6 mx-auto flex w-2/3 flex-row gap-6 rounded-3xl bg-white p-6 text-hsa-gray-300 shadow-md shadow-gray-500">
-      <div className="flex flex-col justify-center p-4 text-center align-middle font-songMyung">
-        <Image src={hsa7} width={300} alt="Decoration" />
-        <div className="mt-4 w-full px-4 text-5xl/10">{date}</div>
-        <div className="mb-4 text-4xl">{time}</div>
-        <Image className="rotate-180" src={hsa7} width={300} alt="Decoration" />
+    <div className="m-4 mx-auto flex w-full max-w-4xl flex-col gap-6 rounded-3xl bg-white p-6 text-hsa-gray-300 shadow-md shadow-gray-500 md:flex-row">
+      <div className="flex flex-col items-center justify-center p-4 text-center font-songMyung">
+        <Image
+          src={hsa7}
+          width={200}
+          height={100}
+          alt="Decoration"
+          className="w-2/3 md:w-[300px]"
+        />
+        <div className="mt-4 w-full px-4 text-3xl md:text-5xl">{date}</div>
+        <div className="mb-4 text-2xl md:text-4xl">{time}</div>
+        <Image
+          className="w-2/3 rotate-180 md:w-[300px]"
+          src={hsa7}
+          width={200}
+          height={100}
+          alt="Decoration"
+        />
       </div>
-      <div className="w-6 rounded-xl bg-hsa-pink-400" />
-      <div className="my-auto flex flex-col gap-2 p-4">
-        <div className="font-songMyung text-5xl">{title}</div>
-        <div className="text-xl font-semibold">Location: {location}</div>
-        <div className="p-2 pl-0 text-xl">{description}</div>
+      <div className="hidden w-6 rounded-xl bg-hsa-pink-400 md:block" />
+      <div className="my-auto flex flex-col gap-2 p-4 text-center md:text-left">
+        <div className="font-songMyung text-3xl md:text-5xl">{name}</div>
+        <div className="text-lg font-semibold md:text-xl">
+          Location: {location}
+        </div>
+        <div className="p-2 pl-0 text-lg md:text-xl">{description}</div>
       </div>
     </div>
   );
