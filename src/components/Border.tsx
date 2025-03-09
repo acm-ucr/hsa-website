1bd07e1ec750087e9104918d59eb90943bbe342d
shuuvi <janeleebmes@gmail.com>
2025-03-09 03:30:36 -0700

    npm things

--- src/components/Border.tsx
+++ src/components/Border.tsx
@@ -2,18 +2,20 @@ import Image from "next/image";
 import border from "@/public/assets/border.svg";
 
 const Border = () => {
-  return <div className="mb-5 w-full overflow-hidden">
-  <div className="flex w-full justify-center gap-x-2">
-    {[...Array(2)].map((_, index) => (
-      <Image
-        key={index}
-        src={border}
-        alt="border"
-        className="w-[70%] min-w-[60]"
-      />
-    ))}
-  </div>
-</div>;
+  return (
+    <div className="mb-5 w-full overflow-hidden">
+      <div className="flex w-full justify-center gap-x-2">
+        {[...Array(2)].map((_, index) => (
+          <Image
+            key={index}
+            src={border}
+            alt="border"
+            className="w-[70%] min-w-[60]"
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Border;
