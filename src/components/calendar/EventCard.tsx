ff47961ea37dcbfedb6025b844ca6fb41ef73698
Cole <yoshi88639@gmail.com>
2025-03-09 00:29:12 -0800

    Fixed bug with formatting, and made wider to match figma

--- src/components/calendar/EventCard.tsx
+++ src/components/calendar/EventCard.tsx
@@ -12,19 +12,19 @@ const EventCard = ({ title, location, start, description }: EventProps) => {
     minute: "2-digit",
   });
   return (
-    <div className="m-4 mx-auto flex w-full max-w-4xl flex-col gap-6 rounded-3xl bg-white p-6 text-hsa-gray-300 shadow-md shadow-gray-500 md:flex-row">
+    <div className="m-4 mx-auto flex w-full max-w-6xl flex-col gap-6 rounded-3xl bg-white p-6 text-hsa-gray-300 shadow-md shadow-gray-500 md:flex-row">
       <div className="flex flex-col items-center justify-center p-4 text-center font-songMyung">
         <Image
           src={hsa7}
           width={200}
           height={100}
           alt="Decoration"
-          className="w-2/3 md:w-[300px]"
+          className="w-2/3 md:w-full"
         />
-        <div className="mt-4 w-full px-4 text-3xl md:text-5xl">{date}</div>
-        <div className="mb-4 text-2xl md:text-4xl">{time}</div>
+        <div className="mt-2 w-full px-4 text-3xl md:text-5xl">{date}</div>
+        <div className="mb-2 text-2xl md:text-4xl">{time}</div>
         <Image
-          className="w-2/3 rotate-180 md:w-[300px]"
+          className="w-2/3 rotate-180 md:w-full"
           src={hsa7}
           width={200}
           height={100}
