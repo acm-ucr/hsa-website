7ce2d1cc8a7ce1a5ddd53bfb8db52ec4409515ff
Juan Aguilar <michelleaguilar765@gmail.com>
2025-03-10 13:20:51 -0700

    fixed join us button

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
         />
       </div>
       <HeroCard />
-      <div className="mx-auto mb-12 max-w-44 rounded-full bg-hsa-green-100 pb-3 pt-2 text-center font-openSans text-2xl font-semibold text-hsa-tan-100 hover:bg-green-900 hover:font-bold hover:text-hsa-tan-200 hover:drop-shadow-lg">
+      <div className="mx-auto mb-12 max-w-44 transform rounded-full bg-hsa-green-100 pb-3 pt-2 text-center font-openSans text-2xl font-semibold text-white duration-300 hover:scale-110 hover:bg-green-900 hover:text-white hover:drop-shadow-lg">
         <Link href="https://discord.gg/BMx92Z2bsY">Join us!</Link>
       </div>
       <div className="mb-5 max-w-full bg-hsa-yellow-100 p-4 shadow-md" />
