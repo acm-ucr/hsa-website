723822c234808b8a594291ac7fef13a1898d41b3
Arik Lee <arikmlee@gmail.com>
2025-03-10 15:32:34 -0700

    fixed menu icon and dropdown

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -52,7 +52,9 @@ const Navigation = () => {
       </div>
 
       <div
-        className={`lg:hidden ${isMobileMenuOpen ? "block" : "hidden"} bg-hsa-pink-200 px-6 py-4 text-hsa-tan-100`}
+        className={`lg:hidden absolute left-0 top-16 w-full bg-hsa-pink-200 px-6 py-10 text-hsa-tan-100 z-50 shadow-lg ${
+          isMobileMenuOpen ? "block" : "hidden"
+        }`} 
       >
         {tags.map(({ link, name }) => (
           <div key={link} className="py-2 text-center">
