bc69eec3833c355cf65417278f193e67eaffdc4b
unknown <twu148@ucr.edu>
2025-03-09 18:29:29 -0700

    Event card gcal integration

--- src/components/calendar/EventCard.tsx
+++ src/components/calendar/EventCard.tsx
@@ -1,21 +1,16 @@
 import hsa7 from "@/public/assets/hsa7.svg";
 import Image from "next/image";
+import { EventProps } from "../ui/calendar";
 
-interface EventData {
-  name: string;
-  location: string;
-  date: string;
-  time: string;
-  description: string;
-}
-
-interface EventCardProps {
-  data: EventData;
-}
-
-const EventCard = ({
-  data: { name, location, date, time, description },
-}: EventCardProps) => {
+const EventCard = ({ title, location, start, description }: EventProps) => {
+  const date = new Date(start as string).toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+  });
+  const time = new Date(start as string).toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
   return (
     <div className="m-6 mx-auto flex w-2/3 flex-row gap-6 rounded-3xl bg-white p-6 text-hsa-gray-300 shadow-md shadow-gray-500">
       <div className="flex flex-col justify-center p-4 text-center align-middle font-songMyung">
@@ -26,7 +21,7 @@ const EventCard = ({
       </div>
       <div className="w-6 rounded-xl bg-hsa-pink-400" />
       <div className="my-auto flex flex-col gap-2 p-4">
-        <div className="font-songMyung text-5xl">{name}</div>
+        <div className="font-songMyung text-5xl">{title}</div>
         <div className="text-xl font-semibold">Location: {location}</div>
         <div className="p-2 pl-0 text-xl">{description}</div>
       </div>
