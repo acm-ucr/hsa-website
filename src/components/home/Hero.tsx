aa535cd233bc83bd9227dc4d263d30b10d707c76
daniyalbhat34 <daniyalbhat34@gmail.com>
2025-03-11 11:43:21 -0700

    Fixed merge conflict

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -5,25 +5,28 @@ import * as motion from "motion/react-client";
 const Hero = () => {
   return (
     <div>
-      <div className="flex flex-col items-center py-[7%]">
-        <motion.div
-      className=""
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8 }}
-    >
-        <HeroCard />
-        <motion.div
-        className="mx-auto mb-12 max-w-44 rounded-full bg-hsa-green-100 pb-3 pt-2 text-center font-openSans text-2xl font-semibold text-hsa-tan-100 hover:bg-green-900 hover:font-bold hover:text-hsa-tan-200 hover:drop-shadow-lg"
+      <motion.div
+        className="flex flex-col items-center py-[7%]"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.6, delay: 0.4 }}
       >
-        <Link href="https://discord.gg/BMx92Z2bsY">Join us!</Link>
+        <HeroCard />
+        <motion.div
+          className="mt-[3vw] w-[13vw] rounded-full bg-hsa-green-100 pb-3 pt-2 text-center text-[2vw] font-semibold text-white hover:bg-green-900 md:w-[10vw] md:text-[1.5vw]"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 0.4 }}
+        >
+          <Link href="https://discord.gg/BMx92Z2bsY">Join us!</Link>
+        </motion.div>
       </motion.div>
-          </motion.div>
-      </div>
-      <div className="mb-5 max-w-full bg-hsa-yellow-100 p-4 shadow-md" />
+      <motion.div
+        className="mb-5 max-w-full bg-hsa-yellow-100 p-4 shadow-md"
+        initial={{ opacity: 0, width: 0 }}
+        animate={{ opacity: 1, width: "100%" }}
+        transition={{ duration: 0.8, delay: 0.7 }}
+      />
     </div>
   );
 };
