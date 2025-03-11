9c28c28012cdc7fdf9be730e0840d12c9ee95f90
shuuvi <janeleebmes@gmail.com>
2025-03-11 12:10:18 -0700

    fixed the mobile response for the text and fixed the border mobile response

--- src/components/home/Do.tsx
+++ src/components/home/Do.tsx
@@ -1,11 +1,11 @@
 const Do = () => {
   return (
     <div className="bg-hsatan-100 w-full flex-row rounded-lg">
-      <div className="mb-2 ml-12 mt-14 h-3/4 w-3/4 space-y-5">
-        <div className="font-songMyung text-3xl text-hsa-blue-100 md:text-4xl lg:text-5xl">
+      <div className="mb-2 mt-14 flex flex-col justify-center">
+        <div className="text-center font-songMyung text-5xl text-hsa-blue-100 md:pl-9 md:text-left md:text-4xl lg:text-5xl">
           What We Do
         </div>
-        <div className="ml-1 pb-8 font-openSans text-xs font-semibold text-hsa-gray-300 md:text-base lg:text-xl">
+        <div className="pb-8 pl-10 pr-10 pt-5 text-center font-openSans text-2xl font-semibold text-hsa-gray-300 md:text-left md:text-xl">
           We are the Hmong Student Association (HSA) at the University of
           California, Riverside (UCR), and we need your support! As an
           organization committed to empowering and uniting Hmong students on
