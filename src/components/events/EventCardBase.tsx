40264abb56b535b4ce022da9c9f4220359f08ce3
Ayush <68819610+akothule@users.noreply.github.com>
2025-03-11 12:07:50 -0700

    Shortened Animation Delay

--- src/components/events/EventCardBase.tsx
+++ src/components/events/EventCardBase.tsx
@@ -60,7 +60,7 @@ const EventCardBase = ({
       <motion.div
         className="mb-6 mt-2 text-center font-songMyung text-3xl font-medium text-hsa-blue-100 md:text-6xl"
         variants={titleAnimation}
-        transition={{ ...transition, delay: 0.5 }}
+        transition={{ ...transition, delay: 0.2 }}
       >
         {title}
       </motion.div>
@@ -68,14 +68,14 @@ const EventCardBase = ({
         <motion.div
           className="flex items-center md:mr-5"
           variants={imageAnimation}
-          transition={{ ...transition, delay: 0.5 }}
+          transition={{ ...transition, delay: 0.2 }}
         >
           <Image src={imageURL} width={width} height={height} alt="" />
         </motion.div>
         <motion.p
           className="flex-1 pt-2 text-center font-openSans text-base font-medium text-gray-700 md:text-left md:text-xl"
           variants={shortTextAnimation}
-          transition={{ ...transition, delay: 0.5 }}
+          transition={{ ...transition, delay: 0.2 }}
         >
           {shortDescription}
         </motion.p>
@@ -83,7 +83,7 @@ const EventCardBase = ({
       <motion.p
         className="mt-6 text-center font-openSans text-base font-medium text-gray-700 md:ml-2 md:mr-16 md:text-left md:text-xl"
         variants={longTextAnimation}
-        transition={{ ...transition, delay: 0.5 }}
+        transition={{ ...transition, delay: 0.2 }}
       >
         {longDescription}
       </motion.p>
